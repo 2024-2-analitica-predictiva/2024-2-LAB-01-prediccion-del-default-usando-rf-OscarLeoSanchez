aaf674bf6e3a58275d608658f1ac93a05146a0f4
OscarLeoSanchez <olsanchezco@unal.edu.co>
2025-01-04 14:56:38 -0500

    Homework finished

--- homework/homework.py
+++ homework/homework.py
@@ -1,3 +1,5 @@
+# homework\homework.py
+
 # flake8: noqa: E501
 #
 # En este dataset se desea pronosticar el default (pago) del cliente el próximo
@@ -91,4 +93,168 @@
 #
 # {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
 # {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
-#
+
+
+# Paso 1
+import pandas as pd
+
+train_data = pd.read_csv("files/input/train_data.csv.zip")
+test_data = pd.read_csv("files/input/test_data.csv.zip")
+
+train_data.rename(columns={"default payment next month": "default"}, inplace=True)
+test_data.rename(columns={"default payment next month": "default"}, inplace=True)
+
+train_data.drop(columns=["ID"], inplace=True)
+test_data.drop(columns=["ID"], inplace=True)
+
+train_data.dropna(inplace=True)
+test_data.dropna(inplace=True)
+
+train_data.loc[train_data["EDUCATION"] > 4, "EDUCATION"] = 4
+test_data.loc[test_data["EDUCATION"] > 4, "EDUCATION"] = 4
+
+train_data = train_data.query("MARRIAGE > 0 and EDUCATION > 0")
+test_data = test_data.query("MARRIAGE > 0 and EDUCATION > 0")
+
+
+# Paso 2
+X_train = train_data.drop(columns=["default"])
+y_train = train_data["default"]
+X_test = test_data.drop(columns=["default"])
+y_test = test_data["default"]
+
+
+# Paso 3
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+categorical_features = ["SEX", "EDUCATION", "MARRIAGE"]
+
+preprocessor = ColumnTransformer(
+    transformers=[("cat", OneHotEncoder(dtype=int), categorical_features)],
+    remainder="passthrough",
+)
+
+pipeline = Pipeline(
+    [
+        ("preprocessor", preprocessor),
+        ("classifier", RandomForestClassifier(random_state=42)),
+    ]
+)
+
+pipeline.fit(X_train, y_train)
+print("Precisión:", pipeline.score(X_test, y_test))
+
+
+# Paso 4
+from sklearn.model_selection import GridSearchCV
+
+param_grid = {
+    "classifier__n_estimators": [200, 250],
+    "classifier__max_depth": [10, None],
+    "classifier__min_samples_split": [10],
+    "classifier__min_samples_leaf": [4],
+    "classifier__max_features": ["auto", "sqrt"],
+    "classifier__bootstrap": [True, False],
+}
+
+grid_search = GridSearchCV(
+    pipeline,
+    param_grid,
+    cv=10,
+    scoring="balanced_accuracy",
+    n_jobs=-1,
+    refit=True,
+    verbose=True,
+)
+
+grid_search.fit(X_train, y_train)
+
+
+# Paso 5
+import joblib
+import os
+import pickle
+import gzip
+
+os.makedirs("files/models", exist_ok=True)
+with gzip.open("files/models/model.pkl.gz", "wb") as f:
+    pickle.dump(grid_search, f)
+
+
+# Paso 6
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    f1_score,
+    balanced_accuracy_score,
+)
+import json
+
+y_train_pred = grid_search.predict(X_train)
+y_test_pred = grid_search.predict(X_test)
+
+metrics = [
+    {
+        "type": "metrics",
+        "dataset": "train",
+        "precision": float(precision_score(y_train, y_train_pred)),
+        "balanced_accuracy": float(balanced_accuracy_score(y_train, y_train_pred)),
+        "recall": float(recall_score(y_train, y_train_pred)),
+        "f1_score": float(f1_score(y_train, y_train_pred)),
+    },
+    {
+        "type": "metrics",
+        "dataset": "test",
+        "precision": float(precision_score(y_test, y_test_pred)),
+        "balanced_accuracy": float(balanced_accuracy_score(y_test, y_test_pred)),
+        "recall": float(recall_score(y_test, y_test_pred)),
+        "f1_score": float(f1_score(y_test, y_test_pred)),
+    },
+]
+
+
+# Paso 7
+from sklearn.metrics import confusion_matrix
+import os
+
+train_cm = confusion_matrix(y_train, y_train_pred)
+test_cm = confusion_matrix(y_test, y_test_pred)
+
+confusion_matrices = [
+    {
+        "type": "cm_matrix",
+        "dataset": "train",
+        "true_0": {
+            "predicted_0": int(train_cm[0, 0]),
+            "predicted_1": int(train_cm[0, 1]),
+        },
+        "true_1": {
+            "predicted_0": int(train_cm[1, 0]),
+            "predicted_1": int(train_cm[1, 1]),
+        },
+    },
+    {
+        "type": "cm_matrix",
+        "dataset": "test",
+        "true_0": {
+            "predicted_0": int(test_cm[0, 0]),
+            "predicted_1": int(test_cm[0, 1]),
+        },
+        "true_1": {
+            "predicted_0": int(test_cm[1, 0]),
+            "predicted_1": int(test_cm[1, 1]),
+        },
+    },
+]
+
+output_file = "files/output/metrics.json"
+os.makedirs("files/output", exist_ok=True)
+
+output_data = metrics + confusion_matrices
+
+with open(output_file, "w") as f:
+    for item in output_data:
+        f.write(str(item).replace("'", '"') + "\n")
